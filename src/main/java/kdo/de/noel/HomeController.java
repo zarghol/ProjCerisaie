1d8a5d3ed2de8fe433092a2846f372d11b7b9ba9
godifreyI <blackIII@hotmail.fr>
2014-05-19 09:40:27 +0200

    Controleur

--- src/main/java/kdo/de/noel/HomeController.java
+++ src/main/java/kdo/de/noel/HomeController.java
@@ -1,15 +1,21 @@
 package kdo.de.noel;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kdo.de.meserreurs.MonException;
+import kdo.de.utilitaires.JBossContext;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
 
 /**
  * Handles requests for the application home page.
@@ -17,23 +23,65 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+
+	@RequestMapping(value = "index.htm")
+	public ModelAndView afficheindex(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return new ModelAndView("Accueil");
+	}
+
+	@RequestMapping(value = "Clients.htm")
+	public ModelAndView clients(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String destinationPage = "";
+		List<Client> mesClients = null;
+
+		// On appelle notre EJB
+		Context ctxt = JBossContext.getInitialContext();
+		GestionClient unclientRemote = (GestionClient) ctxt.lookup("BeanCerisaie");
+		try {
+			mesClients = unclientRemote.listerTousLesClients();
+			destinationPage = "Clients";
+		} catch (MonException e) {
+			destinationPage = "Erreur";
+			request.setAttribute("MesErreurs", e.getCause() + " : " + e.getMessage());
+		}
+		return new ModelAndView(destinationPage, "mesClients", mesClients);
+	}
+
+	@RequestMapping(value = "Sejour.htm")
+	public ModelAndView sejours(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String destinationPage = "";
+		List<Sejour> mesSejour = null;
+
+		// On appelle notre EJB
+		Context ctxt = JBossContext.getInitialContext();
+		GestionClient unclientRemote = (GestionClient) ctxt.lookup("BeanCerisaie");
+		try {
+			mesSejour = unclientRemote.listerTousLesClients();
+			destinationPage = "Sejours";
+		} catch (MonException e) {
+			destinationPage = "Erreur";
+			request.setAttribute("MesErreurs", e.getCause() + " : " + e.getMessage());
+		}
+		return new ModelAndView(destinationPage, "mesSejour", mesSejour);
+	}
+	
+	@RequestMapping(value = "ActivitesSportives.htm")
+	public ModelAndView activitesSportives(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String destinationPage = "";
+		List<ActiSport> mesActiSport = null;
+
+		// On appelle notre EJB
+		Context ctxt = JBossContext.getInitialContext();
+		GestionClient unclientRemote = (GestionClient) ctxt.lookup("BeanCerisaie");
+		try {
+			mesActiSport = unclientRemote.listerTousLesClients();
+			destinationPage = "ActivitesSportive";
+		} catch (MonException e) {
+			destinationPage = "Erreur";
+			request.setAttribute("MesErreurs", e.getCause() + " : " + e.getMessage());
+		}
+		return new ModelAndView(destinationPage, "mesActiSport", mesActiSport);
 	}
 	
 }
